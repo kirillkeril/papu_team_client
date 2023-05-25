00a1894bcfe7de6dbe534c9b428f92cbf2b9a30a
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-05-25 21:08:01 +0300

    сделал роутер

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import {LoginPage} from "./pages/loginPage";
+import { LoginPage } from "./pages/loginPage";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import React, { Component } from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <LoginPage/>
-        </div>
-    );
+  return (
+    // <div className="App">
+    //   <LoginPage />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/LoginPage" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
