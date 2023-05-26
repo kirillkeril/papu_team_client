34f8eaa5e0953b96a94a5cce2244b77bac28e9df
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-05-26 13:43:43 +0300

    svg добавил

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,16 +1,26 @@
-import styles from './landing.module.css';
-import {Header} from "../../components/Header/Header";
-import {useNavigate} from "react-router-dom";
-import {useContext} from "react";
+import styles from "./landing.module.css";
+import { Header } from "../../components/Header/Header";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 import Store from "../../store/store";
-export const MainPage = () => {
-    const store = useContext(Store);
-    return (
-        <div className={styles.page}>
-            <Header/>
-            <main>
+import bannerImg from "../../../public/bannerImg.svg";
 
-            </main>
-        </div>
-    );
-}
+export const MainPage = () => {
+  const store = useContext(Store);
+  return (
+    <div className={styles.page}>
+      <Header />
+      <main>
+        <div
+          className={styles.bannerJPG}
+          style={{
+            backgroundImage: "url(./source/bannerImg.svg)",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </main>
+    </div>
+  );
+};
