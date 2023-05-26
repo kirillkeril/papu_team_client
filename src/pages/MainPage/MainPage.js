fd3858ffb73166fa5c68f757b212d8058e49b39d
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-05-26 18:47:45 +0500

    we;lnlkqw

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -4,36 +4,38 @@ import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import Store from "../../store/store";
 import { Image } from "../../components/UIKit/Image/Image";
+import {Footer} from "../../components/footer/Footer";
 export const MainPage = () => {
   const store = useContext(Store);
   return (
     <div className={styles.page}>
       <Header />
-      <main className={styles.mainBlock}>
-        <div className={styles.banner}>
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          >
-            <Image
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-              }}
-              width={"100%"}
-              src={"/source/banner.svg"}
-            />
-          </div>
-        </div>
-      </main>
+      {/*<main className={styles.mainBlock}>*/}
+      {/*  <div className={styles.banner}>*/}
+      {/*    <div*/}
+      {/*      style={{*/}
+      {/*        position: "absolute",*/}
+      {/*        top: 0,*/}
+      {/*        left: 0,*/}
+      {/*        right: 0,*/}
+      {/*        bottom: 0,*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      <Image*/}
+      {/*        style={{*/}
+      {/*          position: "absolute",*/}
+      {/*          top: 0,*/}
+      {/*          left: 0,*/}
+      {/*          right: 0,*/}
+      {/*          bottom: 0,*/}
+      {/*        }}*/}
+      {/*        width={"100%"}*/}
+      {/*        src={"/source/banner.svg"}*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</main>*/}
+        <Footer/>
     </div>
   );
 };
