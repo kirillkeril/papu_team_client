a7f6cdfbdc2a2ab30f37eda574612444059e1162
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-05-27 04:24:48 +0300

    asdas
    w

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,33 +5,60 @@ import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../../index";
 import { Input } from "../UIKit/Input/Input";
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
 const Header = observer(() => {
-    const navigate = useNavigate();
-    const {store} = useContext(Context);
-    console.log(store.isAuth);
-    return (
-        <header className={styles.mainContainer}>
-            <div className={styles.logo}>
-                <Image width={'30%'} src={'./source/logo.svg'} />
-                <Input name={'search'} placeholder={'Найти среди 1000 товаров'} style={{ marginLeft: '40px'}}/>
-            </div>
-            <menu className={styles.menuContainer}>
-                <div className={styles.menuItem} onClick={() => navigate('/profile')}>Профиль</div>
-                <div className={styles.menuItem} onClick={() => navigate('/product')}>Продукт</div>
-                <div className={styles.menuItem}>Контакты</div>
-            </menu>
-            <div className={styles.buttonsContainer}>
-                {!store.isAuth && <Button onClick={() => navigate('/login')} id={'auth'}>Вход/Регистрация</Button>}
-                <Button style={{padding: '10px', marginLeft: '16px', marginRight: '16px'}}>
-                    <Image src={'./source/basket.svg'} width={'28px'}/>
-                </Button>
-                <Button style={{padding: '10px', background: 'white', border: '2px solid var(--brandColor)'}}>
-                    <Image src={'./source/heart.svg'} width={'28px'}/>
-                </Button>
-            </div>
-        </header>
-    );
-})
+  const navigate = useNavigate();
+  const { store } = useContext(Context);
+  console.log(store.isAuth);
+  return (
+    <header className={styles.mainContainer}>
+      <div className={styles.logo}>
+        <Image
+          onClick={() => {
+            navigate("/");
+          }}
+          style={{cursor: 'pointer'}}
+          width={"30%"}
+          src={"./source/logo.svg"}
+        />
+        <Input
+          name={"search"}
+          placeholder={"Найти среди 1000 товаров"}
+          style={{ marginLeft: "40px" }}
+        />
+      </div>
+      <menu className={styles.menuContainer}>
+        <div className={styles.menuItem} onClick={() => navigate("/profile")}>
+          Профиль
+        </div>
+        <div className={styles.menuItem} onClick={() => navigate("/product")}>
+          Продукт
+        </div>
+        <div className={styles.menuItem}>Контакты</div>
+      </menu>
+      <div className={styles.buttonsContainer}>
+        {!store.isAuth && (
+          <Button onClick={() => navigate("/login")} id={"auth"}>
+            Вход/Регистрация
+          </Button>
+        )}
+        <Button
+          style={{ padding: "10px", marginLeft: "16px", marginRight: "16px" }}
+        >
+          <Image src={"./source/basket.svg"} width={"28px"} />
+        </Button>
+        <Button
+          style={{
+            padding: "10px",
+            background: "white",
+            border: "2px solid var(--brandColor)",
+          }}
+        >
+          <Image src={"./source/heart.svg"} width={"28px"} />
+        </Button>
+      </div>
+    </header>
+  );
+});
 export default Header;
