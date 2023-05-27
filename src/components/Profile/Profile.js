852b4bc53af6e0f892802f0b96db23e46a3d7e64
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-05-27 13:27:47 +0500

    m

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,6 +9,19 @@ const Profile = () => {
     const {store} = useContext(Context);
     const [products, setProducts] = useState([]);
 
+    const updateUser = async() => {
+
+    }
+
+    const initialState = {
+        name: store.user.name,
+        surname: store.user.surname || '',
+        patronymic: store.user.patronymic || "",
+        email: store.user.email,
+        newPassword: '',
+    }
+    const [newUser, setNewUser] = useState(initialState);
+
     useEffect(() => {
         const getProd = async () => {
             const p = await ProductService.getAll();
@@ -23,16 +36,60 @@ const Profile = () => {
                 <h2 className={styles.aboutText}>ОБ АККАУНТЕ</h2>
                 <div className={styles.profile}>
                     <div className={styles.profileContainer} >
-                        <div className={styles.profileName}>{store.user.email}</div>
+                        <div className={styles.profileName}>{store.user.email || store.user.name}</div>
                         <div className={styles.profileForm}>
-                            <Input style={{width:'464px'}} name={'name'}/>
-                            <Input style={{width:'464px'}} name={'surname'}/>
-                            <Input style={{width:'464px'}} name={'otchestvo'}/>
-                            <Input style={{width:'464px'}} name={'email'}/>
-                            <Input style={{width:'464px'}} name={'newPassword'}/>
+                            <Input style={{width:'464px'}}
+                                   onChange={(e) => {
+                                        setNewUser({...newUser, name: e.target.value})
+                                   }
+                                   }
+                                   value={newUser.name}
+                                   placeholder={'Имя'}
+                                   name={'name'}
+                            />
+                            <Input style={{width:'464px'}}
+                                   onChange={(e) => {
+                                       setNewUser({...newUser, surname: e.target.value})
+                                   }
+                                   }
+                                   value={newUser.surname}
+                                   placeholder={'Фамилия'}
+                                   name={'surname'}
+                            />
+                            <Input style={{width:'464px'}}
+                                   onChange={(e) => {
+                                       setNewUser({...newUser, patronymic: e.target.value})
+                                   }
+                                   }
+                                   value={newUser.patronymic}
+                                   placeholder={'Отчество'}
+                                   name={'patronymic'}
+                            />
+                            <Input style={{width:'464px'}}
+                                   onChange={(e) => {
+                                       setNewUser({...newUser, email: e.target.value})
+                                   }
+                                   }
+                                   value={newUser.email}
+                                   placeholder={'Почта'}
+                                   name={'email'}
+                            />
+                            <Input style={{width:'464px'}}
+                                   onChange={(e) => {
+                                       setNewUser({...newUser, newPassword: e.target.value})
+                                   }
+                                   }
+                                   value={newUser.password}
+                                   placeholder={'Новый пароль'
+                            } type={'password'
+                            } name={'newPassword'}/>
                         </div>
                         <footer className={styles.profileFooter}>
-                            <span className={styles.action}>Применить</span>
+                            <span
+                                onClick={() => {
+
+                                }
+                            } className={styles.action}>Применить</span>
                             <span className={styles.action}>Выйти</span>
                         </footer>
                     </div>
@@ -40,7 +97,10 @@ const Profile = () => {
             </div>
 
             <div className={styles.productsContainer}>
-                {products.length > 0 && <UrProducts products={products}/>}
+                {
+                    products.length > 0 &&
+                    store.user.roles.includes('FARMER') &&
+                    <UrProducts products={products}/>}
             </div>
         </div>
     );
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -10,12 +10,13 @@ class ProductService {
         console.log(res.data)
     }
     async update(id, newProductData) {
-        const res = await api.put(`/product/${id}`, {...newProductData});
+        const res = await api.put(`/products/${id}`, {...newProductData});
         console.log(res.data)
     }
     async addNew(productData) {
-        const res = await api.post(`/product/new`, {...productData});
+        const res = await api.post(`/products/add`, {...productData});
         console.log(res.data)
+        return res.data;
     }
     async delete(id) {
         const res = await api.delete(`/products/${id}`);
